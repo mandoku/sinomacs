fab8ffccf4e0e4be3bbf177e0a67a4aa3f1f7c46
Peter Heslin <pheslin@gmail.com>
2016-04-01 00:40:25 +0100

    GPL v 3

--- tei-html-docs-p5.el
+++ tei-html-docs-p5.el
@@ -6,7 +6,7 @@
 ;; URL: http://www.dur.ac.uk/p.j.heslin/Software/Emacs/Download/tei-html-docs-p5.el
 ;; Version: 2.0
 ;;
-;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.
+;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3, or (at your option) any later version.
 ;;
 ;; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 ;;
