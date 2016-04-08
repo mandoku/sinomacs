76377294b72bc425336f8b7d5a55d556eca2dac9
Peter Heslin <pheslin@gmail.com>
2016-04-08 19:03:30 +0100

    add pdf-tools to extras

--- easymacs-extras.el
+++ easymacs-extras.el
@@ -2,6 +2,9 @@
 
 ;;; PDF-tools as LaTeX viewer
 
+(use-package pdf-tools
+  :ensure t)
+
 ;; Done on demand, since require first time is slooow.
 (add-to-list 'auto-mode-alist
 	     '("\\.pdf\\'" .
@@ -20,3 +23,17 @@
 (setq TeX-view-program-selection'((output-dvi "open")
 				  (output-pdf "PDF Tools")
 				  (output-html "open")))
+
+(defun easymacs-latex-pdf-frame ()
+  (interactive)
+  (let ((output-file (TeX-active-master (TeX-output-extension))))
+    (if (file-exists-p output-file)
+	(progn
+	  (find-file-other-frame output-file)
+	  (revert-buffer))
+      (message "Output file %S does not exist." output-file))))
+
+;; Installed by elpy as a dependency, so we might as well turn it on
+(use-package yasnippet
+  :ensure t
+  :config (yas-global-mode 1))
