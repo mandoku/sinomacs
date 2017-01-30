c706ce52c77478c097bb0e5005e2498d0b7d188d
Christian <chris@mbp4-ubuntu>
2017-01-30 13:03:44 +0900

    works now also if no region and no word are available.

--- lisp/sinomacs-more.el
+++ lisp/sinomacs-more.el
@@ -1,22 +1,29 @@
 
 (defun sinomacs-get-char ())
 
-
 (defun sinomacs-grep (string &optional beg end)
   (interactive
    (if (use-region-p)
        (list nil (region-beginning) (region-end))
      (let ((bds (bounds-of-thing-at-point 'word)))
        (list nil (car bds) (cdr bds)))))
-  (let ((coding-system-for-read 'utf-8)
-	(coding-system-for-write 'utf-8)
-	workOnStringP inputStr)
+  (let (workOnStringP inputStr)
     (setq workOnStringP (if string t nil))
     (setq inputStr
-	  (read-string "Search in current directory for: "
-		       (if workOnStringP string (buffer-substring-no-properties beg end))))
-    (grep (concat grep-program " --color -r -n -e \""  inputStr "\" " (file-name-directory
-								       (or (buffer-file-name)
-									   default-directory))))))
+          (if workOnStringP string
+            (read-string "Search in current directory for: ")))
+    (sinomacs-grep--intern inputStr
+                           (file-name-directory
+                            (or (buffer-file-name)
+                                default-directory))
+                           )))
 
-    
+(defun sinomacs-grep--intern (string grepdir &optional beg end)
+  (let ((coding-system-for-read 'utf-8)
+	(coding-system-for-write 'utf-8))
+    (grep (concat grep-program " --color -r -n -e \""  string "\" " grepdir))))
+
+
+
+;;(set-window-margins win width (cdr (window-margins win)))))
+
